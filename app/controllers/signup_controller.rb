61a4c28c458ac1f8f4ffc7f93d65306854aef8ac
723 <rb.natsumi@gmail.com>
2017-02-07 09:50:12 +0900

    エラー表示

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,25 +1,41 @@
 class SignupController < ApplicationController
   def new
-
+    if session[:msg].nil?
+      @flag = 0
+    else
+      @flag = nil
+      @msg = session[:msg]
+    end
   end
   def create
+    @flag = 0
     name = params["signup"]["user_name"]
     id = params["signup"]["user_id"]
     pass = params["signup"]["user_pass"]
     checkpass = params["signup"]["check_pass"]
+    rescheck = User.find_by_user_id(id)
 
     if name.length == 0 || id.length == 0 || pass.length == 0 || checkpass.length ==0
-      render :new
+      @flag = nil
+      session[:msg] = "*全ての項目を入力してください*"
+      cont = 'signup'
+      act = 'new'
     elsif pass != checkpass
-      render :new
-    end
-    rescheck = User.find_by_user_id(id)
-    if rescheck.nil?
+      @flag = nil
+      session[:msg] = "*パスワードが一致しません*"
+      cont = 'signup'
+      act = 'new'
+    elsif rescheck.nil?
       user = User.new(user_id:id,user_name:name,user_passwd:pass)
       user.save
-      redirect_to(:controller => 'todo',:action => 'index')
+      cont = 'todo'
+      act = 'index'
     else
-      redirect_to(:controller => 'signup',:action => 'new')
+      @flag = nil
+      session[:msg] = "*このアカウントは登録できません*"
+      cont = 'signup'
+      act = 'new'
     end
+    redirect_to(:controller => cont,:action => act)
   end
 end
