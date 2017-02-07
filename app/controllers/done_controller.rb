e648b41a1f26ba934bc75c9115b4e6990ffc77ea
723 <rb.natsumi@gmail.com>
2017-02-07 10:47:42 +0900

    🔧 done/index 作成

--- app/controllers/done_controller.rb
+++ app/controllers/done_controller.rb
@@ -1,2 +1,9 @@
 class DoneController < ApplicationController
+  def index
+    if session[:user_id].nil?
+      redirect_to('/')
+    end
+    @user = session[:user_name]
+    @todo = Todo.where("(user_id = ?) and (tasktype = ?)",session[:user_id],2)
+  end
 end
