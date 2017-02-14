81dc654b3da662d166328b409a331bbe3d97c902
723 <rb.natsumi@gmail.com>
2017-02-14 17:40:37 +0900

    🔧 戻るボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'index#index'
+
   get 'login' => 'login#new'
   post 'login' => 'login#create'
   get 'login/logout' => 'login#logout' , as: :login_logout
